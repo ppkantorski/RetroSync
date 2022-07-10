7db54f17d6cf599199806a1c2f17bea4ab644ec6
Patrick Kantorski <ppkantorski@berkeley.edu>
2022-07-09 18:38:15 -0700

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 snes_classic_ip = '192.168.1.39'
 
 # Desired Retroarch Saves Folder
-RA_SAVES_DIR = '/Users/ppkantorski/Documents/RetroArch/saves'
+RA_SAVES_DIR = '/Users/Username/Documents/RetroArch/saves'

848ddf07502605e35e77467918e2dc578e45aa2b
Patrick Kantorski <ppkantorski@berkeley.edu>
2022-07-11 19:35:41 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Server Presets
-snes_classic_ip = '192.168.1.39'
+snes_classic_ip = 'X.X.X.X'
 snes_classic_port = 22
 
 # Desired Retroarch Saves Folder
-RA_SAVES_DIR = '~/Library/Mobile Documents/com~apple~CloudDocs/RetroArch/saves'
+RA_SAVES_DIR = '/Users/USERNAME/Library/Mobile Documents/com~apple~CloudDocs/RetroArch/saves'
 # Location of SNES Classic ROMs
-RA_STOCK_GAMES_DIR = '~/Library/Mobile Documents/com~apple~CloudDocs/RetroArch/games/snes/Classic'
+RA_STOCK_GAMES_DIR = '/Users/USERNAME/Library/Mobile Documents/com~apple~CloudDocs/RetroArch/games/snes/Classic'

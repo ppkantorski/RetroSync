5e95862feeb8aa98a7fd9ddd841738742e92ecb5
Patrick Kantorski <ppkantorski@berkeley.edu>
2022-07-17 02:26:00 -0700

    more notification formatting

--- app/retro_sync_app.py
+++ app/retro_sync_app.py
@@ -214,7 +214,7 @@ class RetroSyncApp(object):
                     f.write(json.dumps(self.retro_sync_cfg, sort_keys=True, indent=4))
                 #print("Please configure config.json accordingly before running again.")
                 
-                self.notify("RetroSync Config", "SNES Classic IP has been updated. Restart RetroSync to apply changes.")
+                self.notify("RetroSync Config", "SNES Classic IP has been updated.\nRestart RetroSync to apply changes.")
     
     def set_ra_saves_loc(self, sender):
         if sender.title == self.config["set_ra_saves_loc"]:
@@ -251,7 +251,7 @@ class RetroSyncApp(object):
                 with open(f'{data_path}/config.json', 'w') as f:
                     f.write(json.dumps(self.retro_sync_cfg, sort_keys=True, indent=4))
                 
-                self.notify("RetroSync Config", "RetroArch Saves location has been updated.\nRestart RetroSync to apply changes.")
+                self.notify("RetroSync Config", "RetroArch Saves Location has been updated.\nRestart RetroSync to apply changes.")
     
     def set_stock_games_loc(self, sender):
         if sender.title == self.config["set_stock_games_loc"]:
@@ -288,7 +288,7 @@ class RetroSyncApp(object):
                 with open(f'{data_path}/config.json', 'w') as f:
                     f.write(json.dumps(self.retro_sync_cfg, sort_keys=True, indent=4))
                 
-                self.notify("RetroSync Config", "Stock Games location has been updated.\nRestart RetroSync to apply changes.")
+                self.notify("RetroSync Config", "Stock Games Location has been updated.\nRestart RetroSync to apply changes.")
     
     
     def enable_disable_icloud(self, sender):
@@ -313,7 +313,7 @@ class RetroSyncApp(object):
             
             sender.title = self.config["disable_icloud"]
             
-            self.notify("RetroSync Config", "iCloud persistence has been disabled.\nRestart RetroSync to apply changes.")
+            self.notify("RetroSync Config", "iCloud Persistence has been disabled.\nRestart RetroSync to apply changes.")
         
         elif sender.title == self.config["disable_icloud"]:
             

407fff0d9d2b31137321ca52222d5c9d2af80b05
Patrick Kantorski <ppkantorski@berkeley.edu>
2022-07-19 02:27:51 -0700

    Update build.py

--- build.py
+++ build.py
@@ -10,14 +10,16 @@ def install(package):
         os.system(f"pip3 install {package}")
 
 # Import / Install
-packages = ['ftpretty', 'py2app', 'rumps']
+packages = ['ftpretty', 'py2app', 'rumps', 'python-telegram-bot']
 [install(pkg) for pkg in packages]
 
+# Define script path
 script_path = os.path.dirname(os.path.abspath( __file__ ))
 
 # Run setup script
 os.system(f'python3 {script_path}/setup.py py2app -A')
 
+# Clean up directories
 shutil.rmtree(f'{script_path}/build')
 os.system(f'mv {script_path}/dist/RetroSync.app {script_path}/RetroSync.app')
 shutil.rmtree(f'{script_path}/dist')

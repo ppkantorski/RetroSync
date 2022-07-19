16cab52fc1187aef5e41dafb5e581a24a6a45ee3
Patrick Kantorski <ppkantorski@berkeley.edu>
2022-07-19 02:28:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from distutils.core import setup
 APP = ['app/retro_sync_app.py']
-DATA_FILES = ['app/retro_sync_app.py', 'app/icon.icns', 'app/icon_off.icns', 'app/icon_stopping.icns']
+DATA_FILES = [
+    'app/retro_sync_app.py',
+    'app/icon.icns',
+    'app/icon_off.icns',
+    'app/icon_stopping.icns'
+]
 OPTIONS = {
     'argv_emulation': True,
     'iconfile': 'app/icon.icns',
@@ -16,5 +21,5 @@ setup(
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
-    install_requires=['rumps', 'ftpretty']
+    install_requires=['rumps', 'ftpretty', 'python-telegram-bot']
 )
